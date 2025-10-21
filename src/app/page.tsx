02d6b37e9ee1c7e168ee6050aed5909c585d215d
Aditya Chaturvedi <kalelrjbrownboy@gmail.com>
2025-10-21 13:45:36 +0530

    Revert "database"
    This reverts commit bd3bc2456c4effe4542a4504e234b557ffdac0d9.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import prisma from "@/lib/db";
+
 import { Button } from "@/components/ui/button";
-const Page=async ()=>{
-const users=await prisma.user.findMany();
+const Page=()=>{
+
   return(<>
   <div className="min-h-screen min-w-screen flex items-center justify-center">
-   {JSON.stringify(users)}
+    <Button >click me</Button>
   </div>
   </>)
 }
